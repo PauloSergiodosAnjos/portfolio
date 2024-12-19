1dcb92d3e8abe48d0e75a7ea9ac38ad7e379a816
Paulo Sérgio dos Anjos <ngcpaulo1@gmail.com>
2024-12-19 20:04:22 -0300

    feat: adicionando json de projetos para iterar no projectModel, ajuste para adicionar linha pretya em baixo de cada card exceto no ultimo

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import AboutMe from "./components/about_me/AboutMe"
 import Header from "./components/header/Header"
 import ProjectModel from "./components/project_model/ProjectModel"
 import "./index.css"
+import data from "./data.json"
 
 function App() {
   return (
@@ -9,10 +10,19 @@ function App() {
       <Header/>
       <main className="mt-[20%] flex flex-col">
         <AboutMe/>
-        <section className="flex mt-[15rem] flex-col gap-[5rem] justify-center px-[10rem]">
-          <h1 className="text-center text-xl">PROJECTS</h1>
-          <ProjectModel description="KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK" link="SADASDASJDIASDIJASDASDIJADS" projectName="sadasdiasdasd" technologies="asdasdasdasidjasd"/>
-          <ProjectModel description="KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK" link="SADASDASJDIASDIJASDASDIJADS" projectName="sadasdiasdasd" technologies="asdasdasdasidjasd"/>
+        <section className="flex mt-[15rem] flex-col justify-center px-[10rem]">
+          <h1 className="text-center text-xl mb-10">PROJECTS</h1>
+          {data.map((project, i)=> (
+            <div className="" key={i}>
+              <ProjectModel description={project.description} image={project.image} link={project.link} projectName={project.projectName} technologies={project.technologies}/>
+              {i < data.length - 1 && (
+                <div className="flex justify-center">
+                  <div className="border-t border-black my-10 w-[50rem]"></div>
+                </div>
+              )}
+            </div>
+          )
+          )}
         </section>
       </main>
     </>
--- src/components/project_model/ProjectModel.tsx
+++ src/components/project_model/ProjectModel.tsx
@@ -2,20 +2,21 @@ interface ProjectModel {
     projectName: string,
     description: string,
     technologies: string,
-    link: string
+    link: string,
+    image: string
 }
 
-export default function ProjectModel({projectName, description, technologies, link}: ProjectModel) {
+export default function ProjectModel({projectName, description, technologies, link, image}: ProjectModel) {
     return(
         <div className="grid grid-cols-2">
-            <div className="h-[29.563rem] p-5">
+            <div className="h-[29.563rem] p-5 flex flex-col justify-between">
                 <p className="mb-6">{projectName}</p>
                 <p className="w-[20rem] mb-6 break-words text-gray-700">{description}</p>
                 <p className="text-gray-500">{technologies}</p>
-                <p className="mt-[15rem] font-bold">{link}</p>
+                    <p className="font-bold underline decoration-solid cursor-pointer w-fit">{link}</p>
             </div>
             <div className="h-[29.563rem]">
-                <img className="h-full" src="https://www.ferasdodesign.com.br/wp-content/uploads/2023/02/2-paisagem_de_praia_com_mar_azul_e_coqueiro_na_areia_feras_do_design_gratis.jpg" alt="" />
+                <img className="h-full w-full" src={image} alt="" />
             </div>
         </div>
     )
